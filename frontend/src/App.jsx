436e310e5bc27ef63851f144d1f90b1f76b05567
Nafis Sadik <nafissadik91@gmail.com>
2026-08-16 03:03:01 +0600

    Restore Member 2 Book Search and Book Details frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,21 @@
 import {
-  Navigate,
-  Route,
+  BrowserRouter,
   Routes,
-} from "react-router";
+  Route
+} from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-
-import ReadingDiaryPage from "./pages/ReadingDiaryPage";
-import ReadingProgressPage from "./pages/ReadingProgressPage";
-
-// Member 2 - Book Search and Book Details
 import SearchBooks from "./pages/SearchBooks";
 import BookDetails from "./pages/BookDetails";
 
-export default function App() {
+function App() {
   return (
-    <div className="min-h-screen bg-[#f7f2e9]">
-
-      <Navbar />
-
+    <BrowserRouter>
       <Routes>
-
-        {/* Shared Reading Progress */}
-        <Route
-          path="/"
-          element={<ReadingProgressPage />}
-        />
-
-        <Route
-          path="/reading-diary"
-          element={<ReadingDiaryPage />}
-        />
-
-        {/* Member 2 - Book Search */}
-        <Route
-          path="/books"
-          element={<SearchBooks />}
-        />
-
-        {/* Member 2 - Book Details */}
-        <Route
-          path="/books/:id"
-          element={<BookDetails />}
-        />
-
-        {/* Unknown URLs */}
-        <Route
-          path="*"
-          element={
-            <Navigate
-              to="/"
-              replace
-            />
-          }
-        />
-
+        <Route path="/" element={<SearchBooks />} />
+        <Route path="/books/:id" element={<BookDetails />} />
       </Routes>
-
-    </div>
+    </BrowserRouter>
   );
 }
+
+export default App;
